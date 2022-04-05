c9bfd08cd6ea7cbeaf9f595a42b709bbd88e11bc
Fedor Blinov <fedyoiblinov@gmail.com>
2022-04-05 20:28:14 +0300

    структура, реализация пузырьковой сортировки и сортировки вставками + оптимизированная версия фрактальной сортировки

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 import datetime
 import random
 
-
-def split_in_groups(data):
-    item_groups = {}
-
-    for item in data:
-        if item in item_groups:
-            item_groups[item].append(item)
-        else:
-            item_groups[item] = [item]
-
-    res = []
-
-    for group in sorted(item_groups.keys()):
-        res.extend(item_groups[group])
-
-    return res
-
-
-def bubble_sort(data):
-    for i in range(len(data) - 1):
-        for j in range(len(data) - i - 1):
-            if data[j] > data[j + 1]:
-                data[j], data[j + 1] = data[j + 1], data[j]
-
-    return data
-
+from sorters.bubble import bubble_sort
+from sorters.fractal_optimised import fractal_sort
+from sorters.insert import insert_sort
 
 if __name__ == '__main__':
-    ints = [random.randint(0, 10000) for _ in range(1000)]
+    ints = [random.randint(0, 100) for _ in range(1000)]
 
     start = datetime.datetime.now().microsecond
     bubble_sort(ints)
@@ -37,14 +14,18 @@ if __name__ == '__main__':
 
     print(f'bubble sort: {end - start} ms')
 
-    start = datetime.datetime.now().microsecond
-    print(split_in_groups(ints))
-    end = datetime.datetime.now().microsecond
-
-    print(f'amazing sort: {end - start} ms')
+    ints = [random.randint(0, 100) for _ in range(1000)]
 
     start = datetime.datetime.now().microsecond
-    ints.sort()
+    insert_sort(ints)
     end = datetime.datetime.now().microsecond
 
-    print(f'python default sort: {end - start} ms')
+    print(f'insert sort: {end - start} ms')
+
+    ints = [random.randint(0, 100) for _ in range(100)]
+
+    start = datetime.datetime.now().microsecond
+    print(fractal_sort(ints))
+    end = datetime.datetime.now().microsecond
+
+    print(f'fractal sort: {end - start} ms')
---
+++ sorters/bubble.py
@@ -0,0 +1,7 @@
+def bubble_sort(data):
+    for i in range(len(data) - 1):
+        for j in range(len(data) - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+    return data
